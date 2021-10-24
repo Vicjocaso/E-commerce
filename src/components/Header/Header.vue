52ceadd52d9d194e1c586ce3efe6aca52dd91256
victor Jose Calderon <victorjosecaderon@victors-MacBook-Air-2.local>
2021-10-24 00:54:10 -0400

    first upload

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container>
+    <v-app>
+      <v-app-bar max-height="60" max-width="1200">
+        <div>
+          <a href="/">
+            <v-img
+              alt="Sneaker"
+              class="shrink mr-2"
+              contain
+              src="./logo.svg"
+              transition="scale-transition"
+              width="100"
+            />
+          </a>
+        </div>
+        <div id="font" class="d-flex align-center mx-6">
+          <div class="d-flex align-center mx-6">
+            <button>Collections</button>
+          </div>
+          <div class="d-flex align-center mx-6">
+            <button>Men</button>
+          </div>
+          <div class="d-flex align-center mx-6">
+            <button>Women</button>
+          </div>
+          <div class="d-flex align-center mx-6">
+            <button>About</button>
+          </div>
+          <div class="d-flex align-center mx-6">
+            <button>Contact</button>
+          </div>
+        </div>
+        <v-spacer></v-spacer>
+        <div class="d-flex align-center">
+          <div class="d-flex align-center mx-6">
+            <v-icon>
+              mdi-cart
+            </v-icon>
+          </div>
+          <a>
+            <v-img
+              alt="Sneaker"
+              class="shrink mr-2"
+              contain
+              src="./image-avatar.png"
+              transition="scale-transition"
+              width="30"
+            />
+          </a>
+        </div>
+      </v-app-bar>
+      <Product />
+    </v-app>
+  </v-container>
+</template>
+
+<script>
+import Product from "../Product/Product.vue";
+
+export default {
+  data: () => ({
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ],
+  }),
+  components: {
+    Product,
+  },
+};
+</script>
+
+<style>
+#font {
+  font-family: "Courier New", Courier, monospace;
+}
+</style>
---
+++ src/components/Product/Product.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card id="center" class="mx-auto my-12" max-width="374">
+    <v-img height="250" src="./image-product-1.jpg"></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    selection: 1,
+  }),
+
+  methods: {
+    reserve() {
+      this.loading = true;
+
+      setTimeout(() => (this.loading = false), 2000);
+    },
+  },
+};
+</script>
+
+<style>
+#center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+</style>
